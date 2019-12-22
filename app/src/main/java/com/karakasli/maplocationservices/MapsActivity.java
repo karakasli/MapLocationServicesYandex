7030ac22d10674b57208f599effe96afcff7bf47
M. Salih Karakaşlı <msalihkarakasli@gmail.com>
2019-12-22 23:14:11 +0300

    Project is committed.

---
+++ app/src/main/java/com/karakasli/maplocationservices/MapsActivity.java
@@ -0,0 +1,256 @@
+package com.karakasli.maplocationservices;
+
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.PointF;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKit;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.layers.ObjectEvent;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.Cluster;
+import com.yandex.mapkit.map.ClusterListener;
+import com.yandex.mapkit.map.ClusterTapListener;
+import com.yandex.mapkit.map.CompositeIcon;
+import com.yandex.mapkit.map.IconStyle;
+import com.yandex.mapkit.map.MapObjectCollection;
+import com.yandex.mapkit.map.RotationType;
+import com.yandex.mapkit.mapview.MapView;
+import com.yandex.mapkit.user_location.UserLocationLayer;
+import com.yandex.mapkit.user_location.UserLocationObjectListener;
+import com.yandex.mapkit.user_location.UserLocationView;
+import com.yandex.runtime.image.ImageProvider;
+import com.yandex.mapkit.map.ClusterizedPlacemarkCollection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+public class MapsActivity extends FragmentActivity implements UserLocationObjectListener, ClusterListener, ClusterTapListener
+{
+    private static final String TAG = MapsActivity.class.getSimpleName();
+
+    private final String MAPKIT_API_KEY = "a08e6973-55fe-46ac-ab27-e4308a0209f0";
+
+    private Location mLastKnownLocation;
+
+    private boolean mLocationPermissionGranted;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private static final int DEFAULT_ZOOM = 15;
+
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+
+    private MapView mapView;
+    private UserLocationLayer userLocationLayer;
+    private ImageButton myLocationBtn;
+
+    private static final int PLACEMARKS_NUMBER = 100;
+
+    private final List<Point> CLUSTER_CENTERS = Arrays.asList(
+            new Point(41.852, 29.204),
+            new Point(39.852, 28.204)
+    );
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+        MapKitFactory.initialize(this);
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_maps);
+
+        myLocationBtn = findViewById(R.id.myLocationBtn);
+        mapView = findViewById(R.id.mapview);
+
+        MapKit mapKit = MapKitFactory.getInstance();
+        userLocationLayer = mapKit.createUserLocationLayer(mapView.getMapWindow());
+        userLocationLayer.setVisible(true);
+        userLocationLayer.setHeadingEnabled(true);
+        userLocationLayer.setObjectListener(this);
+
+        myLocationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                getDeviceLocation();
+            }
+        });
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        getDeviceLocation();
+
+        ClusterizedPlacemarkCollection clusterizedCollection = mapView.getMap().getMapObjects().addClusterizedPlacemarkCollection(this);
+
+        ImageProvider imageProvider = ImageProvider.fromResource(this, R.drawable.pin);
+
+        List<Point> points = createPoints(PLACEMARKS_NUMBER);
+        clusterizedCollection.addPlacemarks(points, imageProvider, new IconStyle());
+
+        clusterizedCollection.clusterPlacemarks(10, 10);
+    }
+
+    @Override
+    protected void onStop() {
+        // Activity onStop call must be passed to both MapView and MapKit instance.
+        mapView.onStop();
+        MapKitFactory.getInstance().onStop();
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        // Activity onStart call must be passed to both MapView and MapKit instance.
+        super.onStart();
+        MapKitFactory.getInstance().onStart();
+        mapView.onStart();
+    }
+
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+    }
+
+    private void getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (mLocationPermissionGranted) {
+                Task locationResult = mFusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            mLastKnownLocation = (Location) task.getResult();
+
+                            mapView.getMap().move(new CameraPosition(new Point(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()), DEFAULT_ZOOM, 0, 0), new Animation(Animation.Type.LINEAR, 1f), null);
+                        } else {
+                            Log.d(TAG, "Current location is null. Using defaults.");
+                            Log.e(TAG, "Exception: %s", task.getException());
+                            mapView.getMap().move(new CameraPosition(new Point(0, 0), DEFAULT_ZOOM, 0, 0), new Animation(Animation.Type.LINEAR, 1f), null);
+                        }
+                    }
+                });
+            }
+            getLocationPermission();
+        } catch(SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+    @Override
+    public void onObjectAdded(@NonNull UserLocationView userLocationView)
+    {
+        userLocationLayer.setAnchor(
+                new PointF((float)(mapView.getWidth() * 0.5), (float)(mapView.getHeight() * 0.5)),
+                new PointF((float)(mapView.getWidth() * 0.5), (float)(mapView.getHeight() * 0.83)));
+
+        userLocationView.getArrow().setIcon(ImageProvider.fromResource(
+                this, R.drawable.user_arrow));
+
+        CompositeIcon pinIcon = userLocationView.getPin().useCompositeIcon();
+
+        pinIcon.setIcon(
+                "pin",
+                ImageProvider.fromResource(this, R.drawable.search_result),
+                new IconStyle().setAnchor(new PointF(0.5f, 0.5f))
+                        .setRotationType(RotationType.ROTATE)
+                        .setZIndex(1f)
+                        .setScale(0.5f)
+        );
+
+        userLocationView.getAccuracyCircle().setFillColor(Color.BLUE);
+    }
+
+    @Override
+    public void onObjectRemoved(@NonNull UserLocationView userLocationView)
+    {
+
+    }
+
+    @Override
+    public void onObjectUpdated(@NonNull UserLocationView userLocationView, @NonNull ObjectEvent objectEvent)
+    {
+
+    }
+
+    @Override
+    public void onClusterAdded(@NonNull Cluster cluster)
+    {
+        // We setup cluster appearance and tap handler in this method
+        cluster.getAppearance().setIcon(new TextImageProvider(Integer.toString(cluster.getSize()), MapsActivity.this));
+        cluster.addClusterTapListener(this);
+    }
+
+    @Override
+    public boolean onClusterTap(@NonNull Cluster cluster)
+    {
+        return false;
+    }
+
+    private List<Point> createPoints(int count) {
+        ArrayList<Point> points = new ArrayList<Point>();
+        Random random = new Random();
+
+        for (int i = 0; i < count; ++i) {
+            Point clusterCenter = CLUSTER_CENTERS.get(random.nextInt(CLUSTER_CENTERS.size()));
+            double latitude = clusterCenter.getLatitude() + Math.random() - 0.5;
+            double longitude = clusterCenter.getLongitude() + Math.random() - 0.5;
+
+            points.add(new Point(latitude, longitude));
+        }
+
+        return points;
+    }
+}
